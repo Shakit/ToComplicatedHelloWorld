5107f808a34ee858b53e9d68a47d55bfb6830443
Shakit <dshakit@gmail.com>
2015-04-05 19:29:45 +0200

    creation de main.cpp avec un Hello World classique.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*============================================================
+  main.cpp
+  Author : Guillaume LEGRU
+  Date : 05/04/2015
+
+  ToComplicatedHelloWorld/main.cpp
+  ============================================================*/
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello World !"
+			  << std::endl;
+	
+	return 0;
+}
